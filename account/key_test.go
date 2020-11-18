0058ed4865a8c3a090efc2a4628d18a6cb9acec0
chris <chrishounsom@icloud.com>
2020-11-18 21:33:46 +0000

    Use test fixture in TestSign

--- account/key_test.go
+++ account/key_test.go
@@ -199,17 +199,22 @@ func TestZero(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
-	toSign := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
+	toSign := []byte{144, 88, 241, 72, 58, 165, 101, 84, 27, 223, 99, 42, 219, 200, 216, 141, 88, 19, 158, 86, 121, 6, 130, 26, 23, 68, 47, 90, 6, 69, 156, 112}
 
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	require.NoError(t, err)
+	byt, _ := hex.DecodeString("1fe8f1ad4053326db20529257ac9401f2e6c769ef1d736b8c2f5aba5f787c72b")
+	key := &ecdsa.PrivateKey{
+		PublicKey: ecdsa.PublicKey{
+			Curve: secp256k1.S256(),
+		},
+		D: new(big.Int).SetBytes(byt),
+	}
 
-	want, err := secp256k1.Sign(toSign, key.D.Bytes())
-	require.NoError(t, err)
+	wantSig, _ := hex.DecodeString("ead3d9a19ac3fb4003c50f2d85e27072dac4e78b77903d6061d8619ba671db0551ab3e72790a7d0c722a3c6ee070a75fa08bb04b7d3ae2ca0be1963bbbdf94c401")
 
 	got, err := Sign(toSign, key)
+
 	require.NoError(t, err)
-	require.Equal(t, want, got)
+	require.Equal(t, wantSig, got)
 
 	// key can be reused
 	require.NotEmpty(t, key)
