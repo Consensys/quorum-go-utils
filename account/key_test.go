0dc245801c6a76064a2a638c36b399e8f187d538
VGLoic <loic.vincent-genod@consensys.net>
2021-09-22 15:45:43 +0200

    update crypto/secp256k1 dependency

--- account/key_test.go
+++ account/key_test.go
@@ -8,7 +8,7 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ConsenSys/quorum/crypto/secp256k1"
+	secp256k1 "github.com/consensys/goquorum-crypto-secp256k1"
 	"github.com/stretchr/testify/require"
 )
 
